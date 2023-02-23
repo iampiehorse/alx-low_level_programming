bb6bdb8d70904bfec4f5c9caf3f4e14ddccd2d4c
iampiehorse <piusemmanuel375@gmail.com>
2023-02-23 13:15:43 -0800

    1-alphabet.c betty correction

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -5,8 +5,7 @@
  */
 void print_alphabet(void)
 {
-	char alphabet;
-	for (alphabet = 'a'; alphabet <= 'z'; alphabet++)
+	char alphabet; for (alphabet = 'a'; alphabet <= 'z'; alphabet++)
 	{
 		_putchar(alphabet);
 	}
